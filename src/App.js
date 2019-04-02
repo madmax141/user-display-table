aa1e3b40b21931fd693435c0d16e5215d2366858
Max Platt <madmax19380@gmail.com>
2019-04-02 14:26:48 -0400

    fetch users on mount, fix error in package json

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = { userData: [] }
+
+  async componentDidMount() {
+    const response = await fetch('https://randomuser.me/api/?results=100');
+    const resultJson = await response.json();
+    console.log(resultJson.results);
+    this.setState({ userData: resultJson.results });
+  }
+
   render() {
     return (
       <div className="App">
